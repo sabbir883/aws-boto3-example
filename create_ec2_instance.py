185b0141b79e70e402211fc4bdfca248b15e9394
sabbir ahmed <sabbir883@yahoo.com>
2016-05-10 12:50:24 -0400

    Added functionality to create a sample ec2 instance.

---
+++ create_ec2_instance.py
@@ -0,0 +1,42 @@
+# Create a t1.micro instance using boto3 SDK. This uses most of the default options. 
+#
+#
+
+
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+instance = ec2.create_instances(
+	
+    DryRun=False,
+    ImageId='ami-f5f41398', #Amazon Linux AMI 2016.03.1 (HVM), SSD Volume Type
+    MinCount=1,
+    MaxCount=1,
+    KeyName='test_key_pair', # Use your configured_key_pair
+    SecurityGroupIds=[ 'sg-7fbf7e04'],
+    UserData='yum install httpd; chkconfig httpd on;/etc/init.d/httpd start',  # This will installed httpd and start the httpd process
+    InstanceType='t2.micro',
+    BlockDeviceMappings=[
+        {
+	    'DeviceName': '/dev/xvda',	    
+            'Ebs': {
+                'SnapshotId': 'snap-a9b8c94e',
+                'VolumeSize': 8,
+                'DeleteOnTermination': True,
+                'VolumeType': 'gp2'
+
+            }
+            
+        }
+    ],
+  
+    
+    InstanceInitiatedShutdownBehavior='stop',
+	IamInstanceProfile={
+        'Arn': 'arn:aws:iam::590487556930:instance-profile/S3_Access'
+    }
+   
+)
+
+
